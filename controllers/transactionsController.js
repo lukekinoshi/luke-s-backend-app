d8d23841d4d59245b8a1d8eec6e8069d0188917f
luke kinoshi <lukekinoshi@lukes-MacBook-Air.local>
2022-09-10 21:28:34 -0400

    backend

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -6,14 +6,17 @@ const { validateURL } = require("../models/validations.js")
 // index Route
 
 transactions.get("/", (req, res) => {
+    // const { id } = req.params;
     res.json(transactionData);
 });
+
+
 // Show Route
-transactions.get("/:arrayIndex", (req, res) => {
+transactions.get("/:index", (req, res) => {
     console.log(req.params);
-    const { arrayIndex } = req.params;
-    if (transactionData[arrayIndex]) {
-        res.json(transactionData[arrayIndex])
+    const { index } = req.params;
+    if (transactionData[index]) {
+        res.json(transactionData[index])
     } else {
         res.status(404).send("no transaction found - sorry");
     } 
@@ -24,16 +27,16 @@ transactions.post("/", validateURL, (req, res) => {
     res.json(transactionData[transactionData.length - 1]);
 });
 // delete Route
-transactions.delete("/:arrayIndex", (req, res)=> {
-    const {arrayIndex} = req.params
-    const deletedtransaction = transactionData.splice(arrayIndex, 1)
-    res.status(200).json(deletedtransaction)
+transactions.delete("/:index", (req, res)=> {
+    const {index} = req.params
+    const deletedTransaction = transactionData.splice(index, 1)
+    res.status(200).json(deletedTransaction)
 });
 // update Route
-transactions.put("/:arrayIndex", (req,res) => {
-    const {arrayIndex} = req.params
-    transactionData[arrayIndex] = req.body
-    res.status(200).json(transactionData[arrayIndex])
+transactions.put("/:index", (req,res) => {
+    const {index} = req.params
+    transactionData[index] = req.body
+    res.status(200).json(transactionData[index])
 });
 
 
